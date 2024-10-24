56a40f743b505b52dee1395e810c878953a68cd0
AngelDLT <angeldev2110@gmail.com>
2024-10-24 14:33:58 -0600

    Feat: Handle login and encrypted passwords

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,10 +1,34 @@
-const db = require('../config/database');
+const db = require("../config/database");
 
 const Usuario = {};
 
-Usuario.registrar = (Nombre, Apellido, Email, Contrasena, Tipo_Usuario, callback) => {
-    const query = 'INSERT INTO Usuarios (Nombre, Apellido, Email, Contraseña, Tipo_Usuario) VALUES (?, ?, ?, ?, ?)';
-    db.query(query, [Nombre, Apellido, Email, Contrasena, Tipo_Usuario], callback);
+// Registrar un nuevo usuario
+Usuario.registrar = (
+  Nombre,
+  Apellido,
+  Email,
+  Contrasena,
+  Tipo_Usuario,
+  callback
+) => {
+  const query =
+    "INSERT INTO Usuarios (Nombre, Apellido, Email, Contrasena, Tipo_Usuario) VALUES (?, ?, ?, ?, ?)";
+  db.query(
+    query,
+    [Nombre, Apellido, Email, Contrasena, Tipo_Usuario],
+    callback
+  );
+};
+
+// Obtener un usuario por Email para el proceso de login
+Usuario.obtenerPorEmail = (Email, callback) => {
+  const query = "SELECT * FROM Usuarios WHERE Email = ?";
+  db.query(query, [Email], (err, result) => {
+    if (err || result.length === 0) {
+      return callback(err, null);
+    }
+    callback(null, result[0]); // Devolver el primer resultado (asume que los emails son únicos)
+  });
 };
 
 module.exports = Usuario;
