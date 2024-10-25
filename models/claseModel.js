eb6c2efd59ef4ff41a24c15862e7fd21607c79d1
chanelXLS <clicea@ucol.mx>
2024-10-25 00:09:43 -0600

    cREACION DE CREARCLASE

--- models/claseModel.js
+++ models/claseModel.js
@@ -3,6 +3,7 @@ const db = require("../config/database");
 
 const Clase = {};
 
+// Función para obtener clases de un profesor por su ID
 Clase.obtenerClasesPorProfesor = (id_profesor, callback) => {
   const query = "SELECT * FROM clases WHERE ID_Profesor = ?";
   db.query(query, [id_profesor], (err, results) => {
@@ -10,6 +11,17 @@ Clase.obtenerClasesPorProfesor = (id_profesor, callback) => {
       return callback(err, null);
     }
     callback(null, results);
+  });
+};
+
+// Nueva función para crear una clase
+Clase.crearClase = (Nombre, ID_Profesor, Horario, callback) => {
+  const query = "INSERT INTO Clases (Nombre, ID_Profesor, Horario) VALUES (?, ?, ?)";
+  db.query(query, [Nombre, ID_Profesor, Horario], (err, result) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, result);
   });
 };
 
--- routes/clasesRoutes.js
+++ routes/clasesRoutes.js
@@ -6,4 +6,7 @@ const clasesController = require("../controllers/clasesController");
 // Ruta para obtener clases de un usuario por su ID de profesor
 router.get("/:id_profesor", clasesController.obtenerClasesPorProfesor);
 
+// Nueva ruta para crear una clase
+router.post("/", clasesController.crearClase);
+
 module.exports = router;
