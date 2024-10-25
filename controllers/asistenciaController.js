14ed81a5aa27657c8055ff8b0d9c42376ca4faa0
AngelDLT <angeldev2110@gmail.com>
2024-10-24 21:18:48 -0600

    Feat: Get attendance by date and class

--- controllers/asistenciaController.js
+++ controllers/asistenciaController.js
@@ -1,17 +1,36 @@
-const Asistencia = require('../models/asistenciaModel');
+const Asistencia = require("../models/asistenciaModel");
 
 exports.registrarAsistencia = (req, res) => {
-    const { ID_Clase, ID_Estudiante, Fecha, Presente } = req.body;
+  const { ID_Clase, ID_Estudiante, Fecha, Presente } = req.body;
 
-    // Agregamos este log para verificar que los datos se están recibiendo correctamente
-    console.log("Datos recibidos:", req.body);
-    
-    Asistencia.registrar(ID_Clase, ID_Estudiante, Fecha, Presente, (err, result) => {
-        if (err) {
-            // Agregamos este log para ver el error de SQL si ocurre
-            console.error("Error en la consulta SQL:", err);
-            return res.status(500).send('Error al registrar la asistencia');
-        }
-        res.status(200).send('Asistencia registrada exitosamente');
-    });
+  // Agregamos este log para verificar que los datos se están recibiendo correctamente
+  console.log("Datos recibidos:", req.body);
+
+  Asistencia.registrar(
+    ID_Clase,
+    ID_Estudiante,
+    Fecha,
+    Presente,
+    (err, result) => {
+      if (err) {
+        // Agregamos este log para ver el error de SQL si ocurre
+        console.error("Error en la consulta SQL:", err);
+        return res.status(500).send("Error al registrar la asistencia");
+      }
+      res.status(200).send("Asistencia registrada exitosamente");
+    }
+  );
+};
+
+exports.obtenerPorFechaYClase = (req, res) => {
+  const { ID_Clase, Fecha } = req.body;
+
+  Asistencia.obtenerPorFechaYClase(ID_Clase, Fecha, (err, result) => {
+    if (err) {
+      console.error("Error en la consulta SQL: ", err);
+      return res.status(500).send("Error al obtener las asistencias");
+    }
+
+    res.status(200).json(result);
+  });
 };
--- models/asistenciaModel.js
+++ models/asistenciaModel.js
@@ -1,10 +1,17 @@
-const db = require('../config/database');
+const db = require("../config/database");
 
 const Asistencia = {};
 
 Asistencia.registrar = (ID_Clase, ID_Estudiante, Fecha, Presente, callback) => {
-    const query = 'INSERT INTO Asistencia (ID_Clase, ID_Estudiante, Fecha, Presente) VALUES (?, ?, ?, ?)';
-    db.query(query, [ID_Clase, ID_Estudiante, Fecha, Presente], callback);
+  const query =
+    "INSERT INTO Asistencia (ID_Clase, ID_Estudiante, Fecha, Presente) VALUES (?, ?, ?, ?)";
+  db.query(query, [ID_Clase, ID_Estudiante, Fecha, Presente], callback);
+};
+
+Asistencia.obtenerPorFechaYClase = (ID_Clase, Fecha, callback) => {
+  const query =
+    "SELECT u.ID_Usuario as ID_Estudiante,u.Nombre, u.Apellido, a.Presente FROM Asistencia a JOIN Usuarios u ON a.ID_Estudiante = u.ID_Usuario WHERE a.ID_Clase = ? AND a.Fecha = ?";
+  db.query(query, [ID_Clase, Fecha], callback);
 };
 
 module.exports = Asistencia;
